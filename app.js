e42f5783275c9721d2383f6c7bf6bb99e1731f83
dawidmarek95x <dawid.marek95x@gmail.com>
2022-10-24 21:55:53 +0200

    Some styles and tags in files for views have been rebuilt

--- app.js
+++ app.js
@@ -9,16 +9,14 @@ const shopRoutes = require("./routes/shop");
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
