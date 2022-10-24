d7cc0e0a56d1982a124ad5e337aa9feddff6eac9
dawidmarek95x <dawid.marek95x@gmail.com>
2022-10-24 23:49:00 +0200

    Use of Pug as Templating Engine

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const PORT = 3000;
@@ -11,11 +14,11 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(PORT, () => {
