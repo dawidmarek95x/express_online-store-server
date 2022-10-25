8d8acff3dd553b1f025e8b68398d5e21bdfeba73
dawidmarek95x <dawid.marek95x@gmail.com>
2022-10-25 21:00:39 +0200

    Use of EJS as Templating Engine

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const rootDir = require("../utils/path");
 const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
