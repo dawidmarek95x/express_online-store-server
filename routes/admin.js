1a5b71b131dcdb1779f1d306936e3417a58562ed
dawidmarek95x <dawid.marek95x@gmail.com>
2022-10-25 21:16:46 +0200

    Removal of unused templating engines

--- routes/admin.js
+++ routes/admin.js
@@ -11,9 +11,6 @@ router.get("/add-product", (req, res, next) => {
   res.render("add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
   });
 });
 
