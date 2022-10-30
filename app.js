2ff90751cc09c0a6e5b0077a6aaa49cf92ced012
dawidmarek95x <dawid.marek95x@gmail.com>
2022-10-30 19:45:06 +0100

    Added products to the products.json file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const express = require("express");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const PORT = 3000;
@@ -14,12 +16,10 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found", path: null });
-});
+app.use(errorController.get404);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
